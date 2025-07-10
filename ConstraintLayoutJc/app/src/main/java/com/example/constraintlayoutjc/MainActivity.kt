75f65d1837bcc906850e70436c0d03371e57b5ae
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2025-07-10 21:56:38 +0530

    initial commit

---
+++ ConstraintLayoutJc/app/src/main/java/com/example/constraintlayoutjc/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.constraintlayoutjc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.example.constraintlayoutjc.ui.theme.ConstraintLayoutJcTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConstraintLayoutJcTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    LearnConstraintLayout()
+                    ConstraintLayoutPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LearnConstraintLayout() {
+    ConstraintLayout {
+        val (redButton, greenButton, blueButton, blackButton) = createRefs()
+
+        Button(onClick = {}, colors = ButtonDefaults.buttonColors(Color.Red), modifier = Modifier.constrainAs(redButton) {
+            top.linkTo(parent.top)
+            width = Dimension.matchParent
+            height = Dimension.value(100.dp)
+        }) {
+            Text(text = "red")
+        }
+
+        Button(onClick = {}, colors = ButtonDefaults.buttonColors(Color.Green), modifier = Modifier.constrainAs(greenButton) {
+            top.linkTo(redButton.bottom)
+        }) {
+            Text(text = "green")
+        }
+
+        Button(onClick = {}, colors = ButtonDefaults.buttonColors(Color.Blue), modifier = Modifier.constrainAs(blueButton) {
+            top.linkTo(redButton.bottom)
+        }) {
+            Text(text = "blue")
+        }
+
+        createHorizontalChain(greenButton, blueButton, chainStyle = ChainStyle.Packed)
+
+        val guideLine = createGuidelineFromBottom(0.01f)
+
+        Button(onClick = {}, colors = ButtonDefaults.buttonColors(Color.Black), modifier = Modifier.constrainAs(blackButton) {
+//            top.linkTo(blueButton.bottom)
+            bottom.linkTo(guideLine)
+        }) {
+            Text(text = "black")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstraintLayoutPreview() {
+    ConstraintLayoutJcTheme {
+        LearnConstraintLayout()
+    }
+}
